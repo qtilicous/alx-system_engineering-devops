44817ffb9da070c03c0efa83ca20cfb0255c45d2
qtilicous <qtilicousbbz@gmail.com>
2024-01-19 03:53:00 -0800

    Fix file 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Puppet manifest to kill a process named killmenow using pkill
 
 exec { 'killmenow_process':
-  command => 'pkill -f killmenow',
-  onlyif  => 'pgrep -f killmenow',
+  command => '/usr/bin/pkill -f killmenow',
+  onlyif  => '/usr/bin/pgrep -f killmenow',
 }
