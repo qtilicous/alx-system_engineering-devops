2ad4b8f4c9e736823037ae1d8a440366f1250208
qtilicous <qtilicousbbz@gmail.com>
2024-01-19 04:05:40 -0800

    still fixing file 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # Puppet manifest to install Flask from pip3
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command  => 'pip3 install Flask==2.1.0',
+  path     => ['/usr/bin'],
+  provider => shell,
+  require  => Package['python3-pip'], # Ensure pip3 is installed first
+}
+
+package { 'python3-pip':
+  ensure => installed,
 }
