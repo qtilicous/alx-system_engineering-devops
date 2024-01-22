ed4d4f346fa9bcb08caa0d69c94d7bc4a17573ab
qtilicous <qtilicousbbz@gmail.com>
2024-01-22 02:35:52 -0800

    Please fix 100-puppet_ssh_confiq.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 # Ensure the SSH client configuration file exists
 
-file { '/home/ubuntu/.ssh/config':
+file { '/etc/ssh/ssh_config':
   ensure => present,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0600',
 }
 
-# Configure SSH client to use the private key ~/.ssh/school
+file_line { 'Turn off passwrd auth':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#PasswordAuthentication',
+}
+
 file_line { 'Declare identity file':
-  path   => '/home/ubuntu/.ssh/config',
-  line   => '    IdentityFile ~/.ssh/school',
-  ensure => present,
-}
-
-# Configure SSH client to refuse password authentication
-file_line { 'Turn off passwd auth':
-  path   => '/home/ubuntu/.ssh/config',
-  line   => '    PasswordAuthentication no',
-  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#IdentityFile',
 }
