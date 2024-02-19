a0e99098cd07fa0054afebc05da40e72a7b18c41
qtilicous <qtilicousbbz@gmail.com>
2024-02-19 11:38:32 -0800

    fix file 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python3
 """
-Script to export data in JSON format from all employees.
+Exports to-do list information of all employees to JSON format.
+
+This script fetches the user information and to-do lists for all employees
+from the JSONPlaceholder API and exports the data to a JSON file.
 """
+
 import json
 import requests
-from sys import argv
+
+
+def fetch_user_data():
+    """Fetch user information and to-do lists for all employees."""
+    url = "https://jsonplaceholder.typicode.com/"
+
+    users = requests.get(url + "users").json()
+
+    data_to_export = {}
+    for user in users:
+        user_id = user["id"]
+        user_url = url + f"todos?userId={user_id}"
+        todo_list = requests.get(user_url).json()
+
+        data_to_export[user_id] = [
+            {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user.get("username"),
+            }
+            for todo in todo_list
+        ]
+
+    return data_to_export
 
 
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/users'
-    response = requests.get(url)
-    users = response.json()
+    data_to_export = fetch_user_data()
 
-    user_dict = {}
-    for user in users:
-        user_id = user.get('id')
-        username = user.get('username')
-
-        response = requests.get('{}/todos?userId={}'.format(url, user_id))
-        tasks = response.json()
-
-        user_tasks = []
-        for task in tasks:
-            task_info = {
-                "username": username,
-                "task": task.get('title'),
-                "completed": task.get('completed'),
-            }
-            user_tasks.append(task_info)
-
-        user_dict[str(user_id)] = user_tasks
-
-    with open('todo_all_employees.json', 'w') as file:
-        json.dump(user_dict, file)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(data_to_export, jsonfile, indent=4)
