a9e94bdb9eddab5e92adca472ce6f8306ff61c34
qtilicous <qtilicousbbz@gmail.com>
2024-03-05 08:51:01 -0800

    Add files README.md and 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+
+    Args:
+        subreddit: A string representing the subreddit to query.
+
+    Returns:
+        The number of subscribers for the given subreddit, or 0 if the
+        subreddit is invalid.
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'my-unique-reddit-user-agent'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        return 0
+    else:
+        data = response.json().get('data')
+        if data:
+            return data.get('subscribers', 0)
+        else:
+            return 0
+
+
+if __name__ == '__main__':
+    print(number_of_subscribers('programming'))
---
+++ README.md
@@ -0,0 +1,7 @@
+This repository contains directories for the following tasks:
+0x00-shell_basics                  0x05-processes_and_signals      0x0A-configuration_management  0x0F-load_balancer          0x15-api
+0x01-shell_permissions             0x06-regular_expressions        0x0B-ssh                       0x10-https_ssl
+0x02-shell_redirections            0x07-networking_basics          0x0C-web_server                0x12-web_stack_debugging_2
+0x03-shell_variables_expansions    0x08-networking_basics_2        0x0D-web_stack_debugging_0     0x13-firewall
+0x04-loops_conditions_and_parsing  0x09-web_infrastructure_design  0x0E-web_stack_debugging_1     0x14-mysql
+0x16-api_advanced
